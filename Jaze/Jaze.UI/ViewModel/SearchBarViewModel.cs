a6d5686e4748fd62925751550f2f023765979f4e
Đào Thanh Tùng <daothanhtung2505@gmail.com>
2017-11-13 20:59:25 +0900

    init layout

---
+++ Jaze/Jaze.UI/ViewModel/SearchBarViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace Jaze.UI.ViewModel
+{
+    class SearchBarViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// The <see cref="MyProperty" /> property's name.
+        /// </summary>
+        public const string MyPropertyPropertyName = "MyProperty";
+
+        private bool _myProperty = false;
+
+        /// <summary>
+        /// Sets and gets the MyProperty property.
+        /// Changes to that property's value raise the PropertyChanged event.
+        /// This property's value is broadcasted by the MessengerInstance when it changes.
+        /// </summary>
+        public bool MyProperty
+        {
+            get
+            {
+                return _myProperty;
+            }
+            set
+            {
+                Set(MyPropertyPropertyName, ref _myProperty, value, true);
+            }
+        }
+    }
+}
