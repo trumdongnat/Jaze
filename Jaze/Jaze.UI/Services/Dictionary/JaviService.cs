1e680dd7eb58b4f5597334511dcbd7c8765a1748
Đào Thanh Tùng <daothanhtung2505@gmail.com>
2018-09-29 10:45:04 +0900

    add seach hanview feature to javi dic

--- Jaze/Jaze.UI/Services/Dictionary/JaviService.cs
+++ Jaze/Jaze.UI/Services/Dictionary/JaviService.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Jaze.Domain;
 using Jaze.UI.Models;
 using Jaze.UI.Util;
@@ -9,235 +11,140 @@ namespace Jaze.UI.Services
 {
     public class JaviService : ServiceBase<JaviModel>
     {
-        //private const string HANVIET_CACHE_FILE = "jv_hv";
-        //private static List<JaviHanVietCache> _javiHanVietCaches;
-        //static JaviService()
-        //{
-        //    _javiHanVietCaches = new List<JaviHanVietCache>();
-        //    string cacheFolder = System.IO.Path.Combine(App.BaseDictionary, "Caches");
-        //    var cachePath = System.IO.Path.Combine(cacheFolder, HANVIET_CACHE_FILE);
-        //    System.IO.Directory.CreateDirectory(cacheFolder);
-        //    if (System.IO.File.Exists(cachePath))
-        //    {
-        //        //read from created cache file
-        //        foreach (var line in System.IO.File.ReadAllLines(cachePath))
-        //        {
-        //            if (!string.IsNullOrWhiteSpace(line))
-        //            {
-        //                try
-        //                {
-        //                    var arr = line.Split('\t');
-        //                    var simpleJavi = new JaviHanVietCache();
-        //                    simpleJavi.DbId = int.Parse(arr[0]);
-        //                    simpleJavi.HanViet = new List<string>();
-        //                    for (int i = 1; i < arr.Length; i++)
-        //                    {
-        //                        simpleJavi.HanViet.Add(arr[i]);
-        //                    }
-        //                    _javiHanVietCaches.Add(simpleJavi);
-        //                }
-        //                catch (Exception)
-        //                {
-        //                    // ignored
-        //                }
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        //read from database
-        //        var db = JazeDatabaseContext.Context;
-        //        var hvDic = db.Kanjis.ToDictionary(kanji => kanji.Word, kanji => kanji.HanViet);
-        //        var javis = db.JaVis.Select(javi => new {javi.Id, javi.Word}).ToList();
-        //        foreach (var javi in javis)
-        //        {
-        //            var simpleJavi = new JaviHanVietCache();
-        //            simpleJavi.DbId = javi.Id;
-        //            simpleJavi.HanViet = new List<string>();
-        //            foreach (var c in javi.Word.ToCharArray())
-        //            {
-        //                var key = c.ToString();
-        //                if (hvDic.ContainsKey(key))
-        //                {
-        //                    simpleJavi.HanViet.Add(hvDic[key]);
-        //                }
-        //            }
-        //            _javiHanVietCaches.Add(simpleJavi);
-        //        }
-        //        //save to cache file
-        //        var writer = System.IO.File.AppendText(cachePath);
-        //        foreach (var simpleJavi in _javiHanVietCaches)
-        //        {
-        //            if (simpleJavi.HanViet.Count > 0)
-        //            {
-        //                writer.Write("{0}\t", simpleJavi.DbId);
-        //                var hvCount = simpleJavi.HanViet.Count;
-        //                for (int i = 0; i < hvCount - 1; i++)
-        //                {
-        //                    writer.Write("{0}\t", simpleJavi.HanViet[i]);
-        //                }
-        //                writer.WriteLine("{0}", simpleJavi.HanViet[hvCount - 1]);
-        //            }
-        //        }
-        //        writer.Close();
-        //    }
-        //    foreach (var simpleJavi in _javiHanVietCaches)
-        //    {
-        //        simpleJavi.HanViet2 = new List<List<string>>();
-        //        foreach (var s in simpleJavi.HanViet)
-        //        {
-        //            simpleJavi.HanViet2.Add(s.Split(',').ToList());
-        //        }
-        //    }
-        //}
-        //public static IEnumerable<JaVi> Search(SearchArgs searchArgs)
-        //{
-        //    var rawKey = searchArgs.SearchKey;
+        private const string HanvietCacheFile = "jv_hv";
+        private static bool _isLoadingCache = false;
+        private static List<JaviHanVietCache> _javiHanVietCaches;
 
-        //    if (string.IsNullOrWhiteSpace(rawKey))
-        //    {
-        //        //return GetAll();
-        //        return null;
-        //    }
-        //    var key = rawKey.Contains("-") ? StringUtil.ConvertRomaji2Katakana(rawKey) : StringUtil.ConvertRomaji2Hiragana(rawKey);
-        //    IEnumerable<JaVi> resultJv = new JaVi[] {};
-        //    IEnumerable<JaVi> resultHv = new JaVi[] { };
-        //    if (StringUtil.IsJapanese(key))
-        //    {
-        //        resultJv = SearchJapanese(new SearchArgs(key, searchArgs.Option));
-        //    }
+        static JaviService()
+        {
+            LoadCachesAsync();
+        }
 
-        //    if (rawKey.Split(' ').All(StringUtil.IsVietnameseWord))
-        //    {
-        //        resultHv = SearchHanViet(searchArgs);
-        //    }
-        //    return resultJv.Union(resultHv);
-        //}
+        private static async void LoadCachesAsync()
+        {
+            await Task.Run(() => LoadCaches());
+        }
 
-        //private static IEnumerable<JaVi> SearchJapanese(SearchArgs searchArgs)
-        //{
-        //    var key = searchArgs.SearchKey;
-        //    switch (searchArgs.Option)
-        //    {
-        //        case SearchOption.Exact:
-        //            return SearchExact(key);
-        //        case SearchOption.StartWith:
-        //            return SearchStartWith(key);
-        //        case SearchOption.EndWith:
-        //            return SearchEndWith(key);
-        //        case SearchOption.Contain:
-        //            return SearchContain(key);
-        //        default:
-        //            throw new ArgumentOutOfRangeException();
-        //    }
-        //}
+        private static void LoadCaches()
+        {
+            _isLoadingCache = true;
+            _javiHanVietCaches = new List<JaviHanVietCache>();
+            string cacheFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Caches");
+            var cachePath = System.IO.Path.Combine(cacheFolder, HanvietCacheFile);
+            System.IO.Directory.CreateDirectory(cacheFolder);
+            if (System.IO.File.Exists(cachePath))
+            {
+                //read from created cache file
+                foreach (var line in System.IO.File.ReadAllLines(cachePath))
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        try
+                        {
+                            var arr = line.Split('\t');
+                            var simpleJavi = new JaviHanVietCache();
+                            simpleJavi.DbId = int.Parse(arr[0]);
+                            simpleJavi.HanViet = new List<string>();
+                            for (int i = 1; i < arr.Length; i++)
+                            {
+                                simpleJavi.HanViet.Add(arr[i]);
+                            }
+                            _javiHanVietCaches.Add(simpleJavi);
+                        }
+                        catch (Exception)
+                        {
+                            // ignored
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //read from database
+                using (var db = new JazeDatabaseContext())
+                {
+                    var hvDic = db.Kanjis.ToDictionary(kanji => kanji.Word, kanji => kanji.HanViet);
+                    var javis = db.JaVis.Select(javi => new { javi.Id, javi.Word }).ToList();
+                    foreach (var javi in javis)
+                    {
+                        var simpleJavi = new JaviHanVietCache();
+                        simpleJavi.DbId = javi.Id;
+                        simpleJavi.HanViet = new List<string>();
+                        foreach (var c in javi.Word.ToCharArray())
+                        {
+                            var key = c.ToString();
+                            if (hvDic.ContainsKey(key))
+                            {
+                                simpleJavi.HanViet.Add(hvDic[key]);
+                            }
+                        }
+                        _javiHanVietCaches.Add(simpleJavi);
+                    }
+                    //save to cache file
+                    var writer = System.IO.File.AppendText(cachePath);
+                    foreach (var simpleJavi in _javiHanVietCaches)
+                    {
+                        if (simpleJavi.HanViet.Count > 0)
+                        {
+                            writer.Write("{0}\t", simpleJavi.DbId);
+                            var hvCount = simpleJavi.HanViet.Count;
+                            for (int i = 0; i < hvCount - 1; i++)
+                            {
+                                writer.Write("{0}\t", simpleJavi.HanViet[i]);
+                            }
+                            writer.WriteLine("{0}", simpleJavi.HanViet[hvCount - 1]);
+                        }
+                    }
+                    writer.Close();
+                }
+            }
+            foreach (var simpleJavi in _javiHanVietCaches)
+            {
+                simpleJavi.HanViet2 = new List<List<string>>();
+                foreach (var s in simpleJavi.HanViet)
+                {
+                    simpleJavi.HanViet2.Add(s.Split(',').ToList());
+                }
+            }
+            _isLoadingCache = false;
+        }
 
-        //private static IEnumerable<JaVi> SearchHanViet(SearchArgs searchArgs)
-        //{
-        //    var key = searchArgs.SearchKey;
-        //    var arr = key.Split(' ');
-        //    var kanjiOfWord = arr.Length;
-        //    var result = new List<JaVi>();
-        //    var db = JazeDatabaseContext.Context;
-        //    foreach (var simpleJavi in _javiHanVietCaches)
-        //    {
-        //        if (simpleJavi.HanViet.Count == kanjiOfWord)
-        //        {
-        //            bool flag = true;
-        //            for (int i = 0; i < kanjiOfWord; i++)
-        //            {
-        //                if (simpleJavi.HanViet2[i].All(hv => hv != arr[i]))
-        //                {
-        //                    flag = false;
-        //                    break;
-        //                }
-        //            }
-        //            if (flag)
-        //            {
-        //                var javi = db.JaVis.Find(simpleJavi.DbId);
-        //                if (javi != null)
-        //                {
-        //                    result.Add(javi);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return result;
-        //    //switch (searchArgs.Option)
-        //    //{
-        //    //    case SearchOption.Exact:
-        //    //        return SearchExact(key);
-        //    //    case SearchOption.StartWith:
-        //    //        return SearchStartWith(key);
-        //    //    case SearchOption.EndWith:
-        //    //        return SearchEndWith(key);
-        //    //    case SearchOption.Contain:
-        //    //        return SearchContain(key);
-        //    //    default:
-        //    //        throw new ArgumentOutOfRangeException();
-        //    //}
+        private List<JaviModel> SearchHanViet(SearchArgs searchArgs)
+        {
+            if (_isLoadingCache) { return new List<JaviModel>(); }
+            var key = searchArgs.SearchKey;
+            var arr = key.Split(' ');
+            var kanjiOfWord = arr.Length;
+            var result = new List<JaviModel>();
+            using (var db = new JazeDatabaseContext())
+            {
+                foreach (var simpleJavi in _javiHanVietCaches)
+                {
+                    if (simpleJavi.HanViet.Count == kanjiOfWord)
+                    {
+                        bool flag = true;
+                        for (int i = 0; i < kanjiOfWord; i++)
+                        {
+                            if (simpleJavi.HanViet2[i].All(hv => hv != arr[i]))
+                            {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag)
+                        {
+                            var javi = db.JaVis.Find(simpleJavi.DbId);
+                            if (javi != null)
+                            {
+                                result.Add(JaviModel.Create(javi));
+                            }
+                        }
+                    }
+                }
+                return result;
+            }
+        }
 
-        //}
-
-        //private static IEnumerable<JaVi> SearchContain(string key)
-        //{
-        //    var db = JazeDatabaseContext.Context;
-        //    if (StringUtil.IsContainKanji(key))
-        //    {
-        //        return db.JaVis.Where(o => o.Word.Contains(key)).ToArray();
-        //    }
-        //    else
-        //    {
-        //        return db.JaVis.Where(o => (o.Kana!=null && o.Word.Contains(key)) || o.Kana.Contains(key)).ToArray();
-        //    }
-        //}
-
-        //private static IEnumerable<JaVi> SearchEndWith(string key)
-        //{
-        //    var db = JazeDatabaseContext.Context;
-        //    if (StringUtil.IsContainKanji(key))
-        //    {
-        //        return db.JaVis.Where(o => o.Word.EndsWith(key)).ToArray();
-        //    }
-        //    else
-        //    {
-        //        return db.JaVis.Where(o => (o.Kana != null && o.Word.EndsWith(key)) || o.Kana.EndsWith(key)).ToArray();
-        //    }
-
-        //}
-
-        //private static IEnumerable<JaVi> SearchStartWith(string key)
-        //{
-        //    var db = JazeDatabaseContext.Context;
-        //    if (StringUtil.IsContainKanji(key))
-        //    {
-        //        return db.JaVis.Where(o => o.Word.StartsWith(key)).ToArray();
-        //    }
-        //    else
-        //    {
-        //        return db.JaVis.Where(o => (o.Kana != null && o.Word.StartsWith(key)) || o.Kana.StartsWith(key)).ToArray();
-        //    }
-
-        //}
-
-        //private static IEnumerable<JaVi> SearchExact(string key)
-        //{
-        //    var db = JazeDatabaseContext.Context;
-        //    if (StringUtil.IsContainKanji(key))
-        //    {
-        //        return db.JaVis.Where(o => o.Word == key).ToArray();
-        //    }
-        //    else
-        //    {
-        //        return db.JaVis.Where(o => o.Word == key || o.Kana == key).ToArray();
-        //    }
-
-        //}
-
-        //private static IEnumerable<JaVi> GetAll()
-        //{
-        //    var db = JazeDatabaseContext.Context;
-        //    return db.JaVis.ToArray();
         public override JaviModel Get(int id)
         {
             using (var db = new JazeDatabaseContext())
@@ -258,7 +165,6 @@ namespace Jaze.UI.Services
 
             if (string.IsNullOrWhiteSpace(rawKey))
             {
-                //return GetAll();
                 return new List<JaviModel>();
             }
             var key = rawKey.Contains("-") ? StringUtil.ConvertRomaji2Katakana(rawKey) : StringUtil.ConvertRomaji2Hiragana(rawKey);
@@ -271,7 +177,7 @@ namespace Jaze.UI.Services
 
             if (rawKey.Split(' ').All(StringUtil.IsVietnameseWord))
             {
-                //TODO resultHv = SearchHanViet(searchArgs);
+                resultHv = SearchHanViet(searchArgs);
             }
             return resultJv.Union(resultHv).ToList();
         }
